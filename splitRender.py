5b48f7e6060def3a157d6c2be9c1629ec457c3e7
Salvatore Merola <salvatore.merol@gmail.com>
2023-04-10 16:39:20 +0200

    add command line parameters for the script

--- splitRender.py
+++ splitRender.py
@@ -1,23 +1,48 @@
+
+import argparse
 import os
 import shutil
 import subprocess
 
 from math import ceil
 
-
+parser = argparse.ArgumentParser()
 # todo parse argline
+parser.add_argument('-i', '--Input', help = 'Input file')
+parser.add_argument('-o', '--Output', help = 'Output folder',  nargs='?', const='./',  default='./', type=str)
 
-file_to_split="../02_stl/HarioSkerton-drillAdapter.stl"
-destination="splitOut" 
-slices_arg = [0, 0, 0]
-plate_size = [20, 20, 20]
-obj_size = [29, 25, 27]
+parser.add_argument('-px', '--plateX', help = 'plate x size',  nargs='?', const=200,  default=200, type=int)
+parser.add_argument('-py', '--plateY', help = 'plate y size',  nargs='?', const=200,  default=200, type=int)
+parser.add_argument('-pz', '--plateZ', help = 'plate z size',  nargs='?', const=200,  default=200, type=int)
 
+parser.add_argument('-sx', '--slicesX', help = 'num of slices along x axis',  nargs='?', const=0,  default=0, type=int)
+parser.add_argument('-sy', '--slicesY', help = 'num of slices along y axis',  nargs='?', const=0,  default=0, type=int)
+parser.add_argument('-sz', '--slicesZ', help = 'num of slices along z axis',  nargs='?', const=0,  default=0, type=int)
+
+parser.add_argument('-ox', '--objectX', help = 'size of the object on x axis',  nargs='?', const=0,  default=0, type=int)
+parser.add_argument('-oy', '--objectY', help = 'size of the object on y axis',  nargs='?', const=0,  default=0, type=int)
+parser.add_argument('-oz', '--objectZ', help = 'size of the object on z axis',  nargs='?', const=0,  default=0, type=int)
+
+args = parser.parse_args()
+
+
+if not args.Input:
+    print("provide an input file")
+    exit()
+
+
+file_to_split = args.Input
+destination=args.Output
+slices_arg = [args.slicesX, args.slicesY, args.slicesZ]
+plate_size = [args.plateX, args.plateY, args.plateZ]
+obj_size = [args.objectX, args.objectY, args.objectZ]
+
+if any(ele == 0 for ele in slices_arg) and any(ele == 0 for ele in obj_size):
+    print ("please specify the number of slices for each axis or the object size")
 
 if not os.path.exists(destination):
    # Create a new directory because it does not exist
    os.makedirs(destination)
-
 
 slices_num = []
 if any(ele == 0 for ele in slices_arg):
@@ -62,6 +87,7 @@ while x <= end_points[0]:
             print (render_stl_array)
             subprocess.call(render_stl_array)
             
+            print (f"created file { os.path.join(destination, filename)}")
             shutil.move(filename, os.path.join(destination, filename))
 
             file_counter += 1
